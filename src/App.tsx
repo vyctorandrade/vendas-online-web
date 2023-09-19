0353ded150e08e4a23ab7d50499097324bb1c9b8
Vyctor Andrade <134069813+vyctorandrade@users.noreply.github.com>
2023-09-19 15:52:08 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const DivMeu = () => {
+  return(
+  <div>
+     <h1>Titulo div meu</h1>
+      <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias quisquam consectetur amet neque ratione sequi? Ea velit quia aspernatur temporibus exercitationem labore eligendi rem vero voluptatibus voluptatem sint, veritatis illum.</h2>
+  </div>
+  )
+}
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    <DivMeu/>
+    </div>
   )
 }
 
